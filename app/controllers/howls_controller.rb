b0ea79ee9ee7d4caaddf17e04ed8f0a269c4d912
a2k11 <adam.kaewell@gmail.com>
2014-10-24 18:03:28 -0400

    Added create method for howl controller created private method for params

--- app/controllers/howls_controller.rb
+++ app/controllers/howls_controller.rb
@@ -1,3 +1,21 @@
 class HowlsController < ApplicationController
 
+  def create
+    howl = Howl.new(howl_params)
+    current_user.howls.create(howl)
+
+    if current_user.save
+      redirect_to :root
+    else
+      render :back
+    end
+
+  end
+
+  private
+
+  def howl_params
+    params.require(:howl).permit(:content)
+  end
+
 end
