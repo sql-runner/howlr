a8f30e0ea0a7bed4f70286b5604ed155f6a3470e
a2k11 <adam.kaewell@gmail.com>
2014-10-24 16:16:05 -0400

    Added username to users table, edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ require "monban/constraints/signed_in"
 require "monban/constraints/signed_out"
 
 Rails.application.routes.draw do
-  resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
-
   constraints Monban::Constraints::SignedIn.new do
     root "dashboards#show", as: :dashboard
   end
@@ -12,5 +9,8 @@ Rails.application.routes.draw do
   constraints Monban::Constraints::SignedOut.new do
     root "sessions#new"
   end
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
 end
