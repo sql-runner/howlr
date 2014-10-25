e2370816c897a9499975cab8ac4dff6024360d8e
a2k11 <adam.kaewell@gmail.com>
2014-10-25 14:51:02 -0400

    Created the method for FollowRelationships controller the create method will add a follower to the user

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,7 +1,10 @@
 class FollowRelationshipsController < ApplicationController
 
   def create
+    user = User.find(params[:id])
+    current_user.followed_users << user
 
+    redirect_to :back
   end
 
 end
