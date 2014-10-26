a5e457f3df458542d59b2d17ef2e35e7f62a3fdd
a2k11 <adam.kaewell@gmail.com>
2014-10-26 13:45:39 -0400

    Added ImageHowls controller

---
+++ app/controllers/image_howls_controller.rb
@@ -0,0 +1,21 @@
+class ImageHowlsController < ApplicationController
+
+  def create
+    image_howl = ImageHowl.new(image_howl_params)
+    @howl = current_user.howls.new(content: image_howl)
+
+    if @howl.save
+      redirect_to dashboard_path
+    else
+      redirect_to dashboard_path
+    end
+
+  end
+
+  private
+
+  def image_howl_params
+    params.require(:image_howl).permit(:avatar)
+  end
+
+end
